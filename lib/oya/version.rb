344cfb41d5ce286f1f56e971aa33d1bdc529d7d7
Masaya Takeda <844196@gmail.com>
2016-02-11 22:49:21 +0900

    v0.2.1

--- lib/oya/version.rb
+++ lib/oya/version.rb
@@ -1,7 +1,7 @@
 module Oya
   AppName = 'Oya'
-  Version = 'v0.2.0'
-  Release = '2016-02-10'
+  Version = 'v0.2.1'
+  Release = '2016-02-11'
 
   # other constant
   Icon = File.expand_path('../../../share/oya/oya_512.png', __FILE__)
